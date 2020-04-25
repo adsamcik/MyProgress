03e96268e023b8329b9638c5140cdacb514cb84b
adsamcik <adsamcik@users.noreply.github.com>
2020-04-25 08:29:43 +0200

    Add plugins

--- windows/flutter/generated_plugin_registrant.cc
+++ windows/flutter/generated_plugin_registrant.cc
@@ -4,6 +4,12 @@
 
 #include "generated_plugin_registrant.h"
 
+#include <file_chooser_plugin.h>
+#include <window_size_plugin.h>
 
 void RegisterPlugins(flutter::PluginRegistry* registry) {
+  FileChooserPluginRegisterWithRegistrar(
+      registry->GetRegistrarForPlugin("FileChooserPlugin"));
+  WindowSizePluginRegisterWithRegistrar(
+      registry->GetRegistrarForPlugin("WindowSizePlugin"));
 }
