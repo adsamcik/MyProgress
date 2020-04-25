c6c2b4724f86d6023734d2e71cf6ee4c1e3615ba
adsamcik <adsamcik@users.noreply.github.com>
2020-04-25 08:20:01 +0200

    Initial commit

---
+++ android/app/src/main/kotlin/com/adsamcik/mark_my_progress/MarkMyProgress/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.adsamcik.mark_my_progress.MarkMyProgress
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ android/app/src/main/kotlin/com/adsamcik/markmyprogress/MarkMyProgress/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.adsamcik.markmyprogress.MarkMyProgress
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
