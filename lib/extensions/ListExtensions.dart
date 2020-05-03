b1ddb95082f95dc0d159b66c401bd947ef248622
adsamcik <adsamcik@users.noreply.github.com>
2020-05-03 11:52:45 +0200

    Add mapNotNull

--- lib/extensions/ListExtensions.dart
+++ lib/extensions/ListExtensions.dart
@@ -1,9 +1,21 @@
 extension ListExtensions<T> on List<T> {
-  List<T> filterWithIndexList( List<int> indexes) {
+  /// Returns list of items that correspond to indexes in [indexes] parameter.
+  List<T> filterWithIndexList(List<int> indexes) {
     var list = List<T>(indexes.length);
     for (var i = 0; i < indexes.length; i++) {
       list[i] = this[indexes[i]];
     }
     return list;
+  }
+
+  /// Maps non-null items to new list
+  List<K> mapNotNull<K>(K Function(T e) mapFunction) {
+    var result = <K>[];
+    forEach((element) {
+      if (element != null) {
+        result.add(mapFunction(element));
+      }
+    });
+    return result;
   }
 }
