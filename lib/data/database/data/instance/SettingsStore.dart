5a7bb188d61686dfda5b978d1a5a0cfbddf93487
adsamcik <adsamcik@users.noreply.github.com>
2020-05-03 11:51:32 +0200

    Add getFilter functions to SettingsStore

--- lib/data/database/data/instance/SettingsStore.dart
+++ lib/data/database/data/instance/SettingsStore.dart
@@ -1,8 +1,30 @@
 import 'package:MarkMyProgress/data/abstract/IPersistentBookmark.dart';
 import 'package:MarkMyProgress/data/database/data/abstract/DatabaseCollection.dart';
 import 'package:MarkMyProgress/data/database/data/abstract/DatabaseProxy.dart';
+import 'package:MarkMyProgress/data/runtime/FilterData.dart';
 import 'package:flutter/cupertino.dart';
+import 'package:sembast/sembast.dart';
 
 class SettingsStore extends DatabaseProxy<String, dynamic> {
   SettingsStore() : super(databasePath: 'settings.db');
+
+  Future<Map<String, dynamic>> getFilterMap() async {
+    var filterDataMap = FilterData().toJson();
+
+    var data = await getAllWithKeys(filterDataMap.keys);
+
+    data.forEach((element) {
+      if (element != null) {
+        filterDataMap[element.key] = element.value;
+      }
+    });
+
+    return filterDataMap;
+  }
+
+  /// Loads filter data from database
+  Future<FilterData> getFilterData() async {
+    var filterDataMap = await getFilterMap();
+    return FilterData.fromJson(filterDataMap);
+  }
 }
