97458dce56b44aaa8dd61c3f11a3c7c447ac8da2
adsamcik <adsamcik@users.noreply.github.com>
2020-05-07 21:09:46 +0200

    Removed local copy of plugins, Removed unused plugins

--- windows/flutter/generated_plugin_registrant.cc
+++ windows/flutter/generated_plugin_registrant.cc
@@ -4,15 +4,12 @@
 
 #include "generated_plugin_registrant.h"
 
-#include <url_launcher_plugin.h>
 #include <file_chooser_plugin.h>
-#include <window_size_plugin.h>
+#include <url_launcher_plugin.h>
 
 void RegisterPlugins(flutter::PluginRegistry* registry) {
-  UrlLauncherPluginRegisterWithRegistrar(
-      registry->GetRegistrarForPlugin("UrlLauncherPlugin"));
   FileChooserPluginRegisterWithRegistrar(
       registry->GetRegistrarForPlugin("FileChooserPlugin"));
-  WindowSizePluginRegisterWithRegistrar(
-      registry->GetRegistrarForPlugin("WindowSizePlugin"));
+  UrlLauncherPluginRegisterWithRegistrar(
+      registry->GetRegistrarForPlugin("UrlLauncherPlugin"));
 }
