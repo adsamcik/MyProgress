412ef160d2662769c9abe1515b5d32af31ec9c31
adsamcik <adsamcik@users.noreply.github.com>
2020-05-14 10:27:53 +0200

    Converted GenericProgress to use freezed

--- lib/data/bookmark/instance/generic_progress.dart
+++ lib/data/bookmark/instance/generic_progress.dart
@@ -1,34 +1,19 @@
 import 'dart:core';
 
 import 'package:MarkMyProgress/data/bookmark/abstract/progress.dart';
-import 'package:json_annotation/json_annotation.dart';
+import 'package:freezed_annotation/freezed_annotation.dart';
 
+part 'generic_progress.freezed.dart';
 part 'generic_progress.g.dart';
 
 /// <summary>
 ///     Generic implementation of progress.
 ///     Should represent most use cases.
 /// </summary>
-@JsonSerializable()
-class GenericProgress implements Progress {
-  GenericProgress(DateTime date, double value) {
-    this.date = date;
-    this.value = value;
-  }
-
-  @override
-  DateTime date;
-
-  @override
-  double value;
+@freezed
+abstract class GenericProgress with _$GenericProgress implements Progress {
+  factory GenericProgress(DateTime date, double value) = _GenericProgress;
 
   factory GenericProgress.fromJson(Map<String, dynamic> json) =>
       _$GenericProgressFromJson(json);
-
-  Map<String, dynamic> toJson() => _$GenericProgressToJson(this);
-
-  @override
-  String toString() {
-    return 'GenericProgress{date: $date, value: $value}';
-  }
 }
